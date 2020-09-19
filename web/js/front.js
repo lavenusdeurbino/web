55d1a40fece52179e45a5d2d6b585065e54995a3
afdezcl <adrifdez_@uma.es>
2020-09-19 15:38:33 +0200

    Cookies working? testing

--- web/js/front.js
+++ web/js/front.js
@@ -11,3 +11,30 @@ $(function () {
         $(this).toggleClass('active');
     });
 });
+
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+    document.getElementById("banner-cookies").style.visibility = "hidden";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+var existCookie = getCookie('GDPRVenus');
+if (existCookie) {
+    document.getElementById("banner-cookies").style.visibility = "hidden";
+}
